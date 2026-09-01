c62d92a924eda37c2f8737ee7e0170431ccd5af9
gimontania <gimontania@gmail.com>
2026-09-01 11:17:42 -0300

    feat: conectar validacion zod a productos

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getMenu, getProduct, createProduct, updateProductController } from "../controllers/product.controller.js"; //importamos el controlador
+import { validateProduct } from "../middlewares/validate-product.js";
 
 const router = Router();
 
 router.get("/menu", getMenu); //ruta para obtener todos los productos
 router.get("/menu/:id", getProduct);
-router.post("/menu", createProduct);
+router.post("/menu", validateProduct, createProduct);
 router.put("/menu/:id", updateProductController);
 
 
